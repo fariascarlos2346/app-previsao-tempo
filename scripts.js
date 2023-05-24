0ed5b6dd8614660709232cd4a54079f26c86b69f
Carlos Farias <fariascarlos2346@gmail.com>
2023-05-24 15:35:51 -0300

    criação de um APP de previsão do tempo com HTML, CSS e Javascript

--- scripts.js
+++ scripts.js
@@ -2,9 +2,24 @@
 
 const key = "8b68ea2758673ff898d434cdbfcc60c8"
 
+function colocarDadosNaTela(dados){
+   console.log(dados)
+   document.querySelector(".cidade").innerHTML = "Tempo em" + dados.name
+   document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+   document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+   document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+   document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+
+async function buscarCidade(cidade){
+   const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+   
+   colocarDadosNaTela(dados)
+}
 
 function cliqueiNoBotao() {
-   const input = document.querySelector(".input-cidade")
+   const cidade = document.querySelector(".input-cidade").value
 
-      console.log(input)
+      buscarCidade(cidade)
 }
